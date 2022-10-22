5825f7ebfd6963f3608d45a4adb48e7bd1cf71c1
andsolo <andso1o1@yandex.ru>
2022-10-23 02:06:59 +0300

    complete full homework 1

---
+++ Homework/hw1/Ex2/Ex2.cs
@@ -0,0 +1,13 @@
+﻿// 2. На вход три числа, на выход максимальное из них 
+int a, b, c, max;
+
+Console.WriteLine("Введите три числа: ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+if (b > max)max = b;
+if (c > max) max = c;
+
+Console.WriteLine("Максимальное число: "+ max);
---
+++ Homework/hw1/Ex4/Ex4.cs
@@ -0,0 +1,15 @@
+﻿// 4. На вход N, на выход все четные числа от 1 до N
+
+int count = 1;
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (count <= num)
+{
+	if (count % 2 ==0)
+	{
+		Console.WriteLine(count);
+	}
+	count ++;
+}
